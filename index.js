547fe5f5210400b7863432ce655bb269c616a8f7
Rasha202 <wcnh1333@gmail.com>
2026-05-11 09:40:46 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
  * @returns {number} The sum of a and b.
  */
 function sum(a, b) {
-  // TODO: Implement this function.
+  return a + b;
 }
 
 /**
@@ -12,7 +12,7 @@ function sum(a, b) {
  * @returns {string} The reversed string.
  */
 function reverseString(str) {
-  // TODO: Implement this function.
+  return str.split("").reverse().join("");
 }
 
 /**
@@ -20,7 +20,11 @@ function reverseString(str) {
  * @returns {number} The largest number in the array. Return null if the array is empty.
  */
 function findLargest(numbers) {
-  // TODO: Implement this function.
+  if (numbers.length === 0) {
+    return null;
+  }
+
+  return Math.max(...numbers);
 }
 
 /**
@@ -30,7 +34,10 @@ function findLargest(numbers) {
  * The check should be case-insensitive.
  */
 function isPalindrome(str) {
-  // TODO: Implement this function.
+  const lowerStr = str.toLowerCase();
+  const reversed = lowerStr.split("").reverse().join("");
+
+  return lowerStr === reversed;
 }
 
 /**
@@ -38,7 +45,7 @@ function isPalindrome(str) {
  * @returns {number[]} A new array containing only the even numbers from the original array.
  */
 function filterEvenNumbers(numbers) {
-  // TODO: Implement this function.
+  return numbers.filter(number => number % 2 === 0);
 }
 
 
@@ -50,4 +57,3 @@ module.exports = {
   isPalindrome,
   filterEvenNumbers,
 };
-
